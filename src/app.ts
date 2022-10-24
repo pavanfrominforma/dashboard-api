4b0ef2833146121889c2241db5850eb33346b912
Pavan Kumar Yekabote <yekabote.kumar@meritgroup.co.uk>
2022-10-25 00:05:02 +0530

    Feat: Added app.ts, initializer file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-import express from 'express';
-import { AppConfig } from './config';
-
+import express from "express";
+import { AppConfig } from "./config";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { V1Router } from "./routes";
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api", V1Router);
 
 app.listen(AppConfig.PORT, () => {
     console.log("Running dashboard server on port " + AppConfig.PORT);
-})
+});
