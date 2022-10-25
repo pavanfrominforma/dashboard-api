b00e43e0fb32ef55ad5c82f1cb0399625080ea1b
Pavan Kumar Yekabote <kumar.pavan@informa.com>
2022-10-25 12:47:51 +0530

    Feat: vdp/feeds route responds with data.

--- src/routes/dashboard.router.ts
+++ src/routes/dashboard.router.ts
@@ -1,14 +1,14 @@
-import { Router, Response, Request } from 'express';
-import { DashboardController } from '../controllers/dashboard.controller';
+import { Router, Response, Request } from "express";
+import { DashboardController } from "../controllers/dashboard.controller";
 
 const router = Router();
 
 router.get("/vdp/feeds", async (req: Request, res: Response) => {
-    try{
+    try {
         const instance = await DashboardController.getInstance();
-        
-
-    }catch(e){ }
+        let response = await instance.getVDPDashboard();
+        res.status(200).json(response);
+    } catch (e) {}
 });
 
 export const DashboardRouter = router;
