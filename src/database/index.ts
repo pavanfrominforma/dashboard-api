9a7425fcbe5df71b6f8fd133500b13fa5ee13b16
Pavan Kumar Yekabote <kumar.pavan@informa.com>
2022-10-25 12:47:09 +0530

    Feat: Database class handles  all database related operations

---
+++ src/database/index.ts
@@ -0,0 +1,61 @@
+import {
+    getConnection,
+    Connection,
+    Result,
+    initOracleClient,
+    OUT_FORMAT_OBJECT,
+} from "oracledb";
+import { DbConnectionError } from "../utils/exceptions";
+import { AppConfig, DbConfig } from "../config";
+
+export class Database {
+    private static db: Database;
+
+    private connection: Connection;
+
+    private constructor(private opts: Database.InitOptions) {
+        this.connection = null as any;
+    }
+
+    private async initialize() {
+        console.log("Config ", DbConfig.connectionString);
+        initOracleClient({ libDir: AppConfig.ORACLE_CONFIG_PATH });
+        this.connection = await getConnection(this.opts);
+    }
+    static async getInstance(opts: Database.InitOptions) {
+        if (Database.db) return Database.db;
+        Database.db = new Database(opts);
+        await Database.db.initialize();
+        return Database.db;
+    }
+
+    static getInstanceInitialized() {
+        return Database.db;
+    }
+
+    public static getConnection(): Connection {
+        if (!Database.db)
+            throw new DbConnectionError(
+                "Connection not initialized, call getInstance method."
+            );
+        return Database.db.connection;
+    }
+
+    public async execute(query: string): Promise<Result<any>> {
+        await this.connection.break();
+        console.log("EXecuting query ", query);
+        const records = await this.connection.execute(query, [], {
+            outFormat: OUT_FORMAT_OBJECT,
+        });
+        console.log("Execution of query complete ");
+        return records;
+    }
+}
+
+export namespace Database {
+    export interface InitOptions {
+        user: string;
+        password: string;
+        connectionString: string;
+    }
+}
