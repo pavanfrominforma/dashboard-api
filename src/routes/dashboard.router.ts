d8e601e74c402ae00b7d40502cc8411fe5198bf2
Pavan Kumar Yekabote <kumar.pavan@informa.com>
2022-10-26 14:07:54 +0530

    Feat: Added caching mechanism to vdp/feeds

--- src/routes/dashboard.router.ts
+++ src/routes/dashboard.router.ts
@@ -1,5 +1,7 @@
 import { Router, Response, Request } from "express";
 import { DashboardController } from "../controllers/dashboard.controller";
+import { Store } from "../utils/store";
+import NodeCache from "node-cache";
 
 const router = Router();
 
@@ -7,6 +9,12 @@ router.get("/vdp/feeds", async (req: Request, res: Response) => {
     try {
         const instance = await DashboardController.getInstance();
         let response = await instance.getVDPDashboard();
+
+        const cachePath = (req as any).cachePath;
+        const cache = Store.get<NodeCache>(Store.KEYS.CACHE);
+        console.log("Saving path " + cachePath + " to cache");
+        cache.set(cachePath, response);
+        
         res.status(200).json(response);
     } catch (e) {}
 });
