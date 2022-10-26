cc18580f997110e51b2fa9c5a0066777f98227ee
Pavan Kumar Yekabote <kumar.pavan@informa.com>
2022-10-26 14:10:23 +0530

    Feat: Add Cache to all routes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { V1Router } from "./routes";
 import { Database } from "./database";
+import NodeCache from "node-cache";
+import { buildCacheMiddleware } from "./middlewares";
+import { Store } from "./utils/store";
 
 (async () => {
     console.log("Initializing database connection...");
@@ -11,9 +14,17 @@ import { Database } from "./database";
     await Database.getInstance(DbConfig);
     console.log("Connected to database completed.");
 
+    const cache = new NodeCache({
+        stdTTL: AppConfig.CACHE_TTL,
+    });
+    Store.set(Store.KEYS.CACHE, cache);
+
     const app = express();
     app.use(cors());
     app.use(bodyParser.json());
+
+    const cacheMiddleware = buildCacheMiddleware();
+    app.use(cacheMiddleware);
 
     app.use("/api", V1Router);
 
