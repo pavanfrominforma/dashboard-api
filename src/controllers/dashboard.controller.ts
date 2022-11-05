fdaac2d3755c31242901508ce3be26f81335e7b2
Pavan Kumar Yekabote <kumar.pavan@informa.com>
2022-11-05 13:40:51 +0530

    Feat: Added filter mapper, feedtype counts

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -1,9 +1,165 @@
 import { Request, Response } from "express";
 import { Connection, initOracleClient } from "oracledb";
 import { Database } from "../database";
+import moment from "moment";
 
 export class DashboardController {
     private db: Database;
+
+    private columns: any = {
+        FEEDNAME: {
+            name: "FEEDNAME",
+            field: "FEEDNAME",
+            datatype: "string",
+            default: "",
+            position: 1,
+            show: true,
+        },
+        FEEDTYPE: {
+            name: "FEEDTYPE",
+            field: "FEEDTYPE",
+            datatype: "string",
+            default: "",
+            show: true,
+            position: 2,
+        },
+        FEEDFREQUENCY: {
+            name: "FEEDFREQUENCY",
+            field: "FEEDFREQUENCY",
+            datatype: "string",
+            default: "",
+            position: 3,
+            show: true,
+        },
+        FILEPATH: {
+            name: "FILEPATH",
+            field: "FILEPATH",
+            datatype: "string",
+            default: "",
+            position: 4,
+            show: true,
+        },
+        FEEDDELIVERYMETHOD: {
+            name: "FEEDDELIVERYMETHOD",
+            field: "FEEDDELIVERYMETHOD",
+            datatype: "string",
+            default: "",
+            position: 5,
+            show: true,
+        },
+        FILENAME: {
+            name: "FILENAME",
+            field: "FILENAME",
+            datatype: "string",
+            default: "",
+            position: 6,
+            show: true,
+        },
+        SENDER: {
+            name: "SENDER",
+            field: "SENDER",
+            datatype: "string",
+            default: "",
+            position: 7,
+            show: true,
+        },
+        DNP: {
+            name: "DNP",
+            field: "DNP",
+            datatype: "string",
+            default: "Y",
+            position: 8,
+            show: false,
+        },
+        LASTLOADEDDATE1: {
+            name: "LASTLOADEDDATE1",
+            field: "LASTLOADEDDATE1",
+            datatype: "date",
+            default: "",
+            position: 9,
+            show: true,
+        },
+        LASTLOADEDCOUNT1: {
+            name: "LASTLOADEDCOUNT1",
+            field: "LASTLOADEDCOUNT1",
+            datatype: "number",
+            default: "",
+            position: 10,
+            show: true,
+        },
+        LASTLOADEDDATE2: {
+            name: "LASTLOADEDDATE2",
+            field: "LASTLOADEDDATE2",
+            datatype: "date",
+            default: "",
+            position: 11,
+            show: true,
+        },
+        LASTLOADEDCOUNT2: {
+            name: "LASTLOADEDCOUNT2",
+            field: "LASTLOADEDCOUNT2",
+            datatype: "number",
+            default: "",
+            position: 12,
+            show: true,
+        },
+        LASTLOADEDDATE3: {
+            name: "LASTLOADEDDATE3",
+            field: "LASTLOADEDDATE3",
+            datatype: "date",
+            default: "",
+            position: 13,
+            show: true,
+        },
+        LASTLOADEDCOUNT3: {
+            name: "LASTLOADEDCOUNT3",
+            field: "LASTLOADDEDCOUNT3",
+            datatype: "number",
+            default: "",
+            position: 14,
+            show: true,
+        },
+        LASTLOADEDDATE4: {
+            name: "LASTLOADEDDATE4",
+            field: "LASTLOADEDDATE4",
+            datatype: "date",
+            default: "",
+            position: 15,
+            show: true,
+        },
+        LASTLOADEDCOUNT4: {
+            name: "LASTLOADEDCOUNT4",
+            field: "LASTLOADEDCOUNT4",
+            datatype: "number",
+            default: "",
+            position: 16,
+            show: true,
+        },
+        LASTLOADEDDATE5: {
+            name: "LASTLOADEDDATE5",
+            field: "LASTLOADEDDATE5",
+            datatype: "date",
+            default: "",
+            position: 17,
+            show: true,
+        },
+        LASTLOADEDCOUNT5: {
+            name: "LASTLOADEDCOUNT5",
+            field: "LASTLOADEDCOUNT5",
+            datatype: "number",
+            default: "",
+            position: 18,
+            show: true,
+        },
+        FEEDSTATUS: {
+            name: "FEEDSTATUS",
+            field: "FEEDSTATUS",
+            datatype: "string",
+            default: "Active",
+            position: 19,
+            show: true,
+        },
+    };
 
     private constructor() {
         this.db = null as any;
@@ -15,36 +171,140 @@ export class DashboardController {
         return instance;
     }
 
-    async getVDPDashboard() {
+    async getVdpDashboardCount(opts: DashboardController.VDPDashboardOpts) {
+        const filters = this.getFilterAsSqlQuery(opts);
+        const filtersJoined = filters.join("and");
+        let filter =
+            filtersJoined || filtersJoined != ""
+                ? " where " + filters.join(" and ")
+                : "";
         const results = await this.db.execute(
-            "select * from vdp_loading_summary"
+            `
+            with vdp as
+            (
+            select feedname,feedtype,feedfrequency,feeddeliverymethod,to_DATE(substr(lastloadeddate1,1,10),'DD-MM-YYYY') lastloadeddate,
+                    ROW_NUMBER() OVER (PARTITION BY feedname,feedtype,feedfrequency,feeddeliverymethod ORDER BY to_DATE(substr(lastloadeddate1,1,10),'DD-MM-YYYY') DESC) AS ROW_NUM
+            from vdp.vls
+            --group by feedname,feedtype,feedfrequency,feeddeliverymethod
+            )
+            ,feed_cnt as
+            (select vls.* from vdp
+            inner join vdp.vls on
+            vdp.feedname = vls.feedname
+            and vdp.feedtype = vls.feedtype
+            and vdp.feedfrequency = vls.feedfrequency
+            and vdp.feeddeliverymethod = vls.feeddeliverymethod
+            and vdp.lastloadeddate = to_DATE(substr(vls.lastloadeddate1,1,10),'DD-MM-YYYY')
+            and vdp.row_num = 1)
+            select feedtype, FeedStatus, count(*) Total from feed_cnt ${filter}
+            group by feedtype, FeedStatus
+            `
+        );
+        console.log("Query exec complete...!");
+        const resultMap = {} as any;
+
+        for (let row of results.rows as any[]) {
+            const feedtype = row.FEEDTYPE;
+            const feedStatus =
+                row.FEEDSTATUS == "Active" ? "active" : "inactive";
+            const total = Number(row.TOTAL);
+
+            if (!resultMap[feedtype])
+                resultMap[feedtype] = {
+                    active: 0,
+                    inactive: 0,
+                };
+
+            resultMap[feedtype][feedStatus] = total;
+        }
+
+        const resultList = Object.keys(resultMap).map((key: string) =>
+            Object({
+                feedtype: key,
+                ...resultMap[key],
+            })
+        );
+        console.log("Results map ", resultList);
+        return resultList;
+    }
+
+    getFilterAsSqlQuery(
+        opts: DashboardController.VDPDashboardOpts,
+        prefix = ""
+    ) {
+        const userFilter: any = opts.filter || {};
+        const filters = [];
+        const filterKeys: string[] = Object.keys(this.columns);
+        for (let key of filterKeys) {
+            const meta = this.columns[key] as any;
+            let value = userFilter[key];
+            if (!value || value === "") continue;
+
+            if (meta.datatype === "string") value = `'${value}'`;
+            else if (meta.datatype == "number") value = value;
+
+            const queryBlock = `${prefix}${key}=${value}`;
+            filters.push(queryBlock);
+        }
+        return filters;
+    }
+    async getVDPDashboard(opts: DashboardController.VDPDashboardOpts) {
+        let filters = this.getFilterAsSqlQuery(opts, "vls.");
+        let filter = "";
+        if (filters.join("") != "") filter = " and " + filters.join(" and ");
+        const results = await this.db.execute(
+            `with vdp as
+            (
+            select feedname,feedtype,feedfrequency,feeddeliverymethod,to_DATE(substr(lastloadeddate1,1,10),'DD-MM-YYYY') lastloadeddate,
+                    ROW_NUMBER() OVER (PARTITION BY feedname,feedtype,feedfrequency,feeddeliverymethod ORDER BY to_DATE(substr(lastloadeddate1,1,10),'DD-MM-YYYY') DESC) AS ROW_NUM
+            from vdp.vls
+            --group by feedname,feedtype,feedfrequency,feeddeliverymethod
+            )
+            
+            select vls.* from vdp
+            inner join vdp.vls on
+            vdp.feedname = vls.feedname
+            and vdp.feedtype = vls.feedtype
+            and vdp.feedfrequency = vls.feedfrequency
+            and vdp.feeddeliverymethod = vls.feeddeliverymethod
+            and vdp.lastloadeddate = to_DATE(substr(vls.lastloadeddate1,1,10),'DD-MM-YYYY')
+            and vdp.row_num = 1
+            ${filter}
+            `
         );
 
-        const fields = results.metaData?.map((field: any) => {
-            let position = "";
-            if (
-                field.name.toLowerCase().indexOf("feed") != -1 &&
-                field.name.toLowerCase().indexOf("name") != -1
-            )
-                position = "first";
-            else if (
-                field.name.toLowerCase().indexOf("status") != -1 &&
-                field.name.toLowerCase().indexOf("feed") != -1
-            )
-                position = "last";
-
-            return {
-                name: field.name,
-                field: field.name,
-                position: position,
-            };
-        });
-
+        const fields = Object.values(this.columns);
         const responseResults = {
             count: results.rows?.length,
             headers: fields,
             data: results.rows,
         };
         return responseResults;
+    }
+}
+
+namespace DashboardController {
+    export interface VDPDashboardOpts {
+        filter: {
+            FEEDNAME: string;
+            FEEDTYPE: string;
+            FEEDFREQUENCY: string;
+            FEEDDELIVERYMETHOD: string;
+            FILENAME: string;
+            SENDER: string;
+            DNP: string;
+            LASTLOADEDDATE1: string;
+            LASTLOADEDCOUNT1: string;
+            LASTLOADEDDATE2: string;
+            LASTLOADEDCOUNT2: string;
+            LASTLOADEDDATE3: string;
+            LASTLOADEDCOUNT3: string;
+            LASTLOADEDDATE4: string;
+            LASTLOADEDCOUNT4: string;
+            LASTLOADEDDATE5: string;
+            LASTLOADEDCOUNT5: string;
+            FEEDSTATUS: string;
+            FILEPATH: string;
+        };
     }
 }
